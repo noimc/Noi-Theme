69cff90cc7a20a13bf7989b5c25bff088f42eba5
Noi MC <yutynetwork2011@gmail.com>
2025-06-25 09:33:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,88 +1,86 @@
 #!/bin/bash
 
-if (( $EUID != 0 )); then
+# Kiểm tra quyền root
+if (( EUID != 0 )); then
     echo "Please run as root"
-    exit
+    exit 1
 fi
 
 clear
 
-installTheme(){
-    cd /var/www/
-    tar -cvf Pterodactyl_Nightcore_Themebackup.tar.gz pterodactyl
+installTheme() {
+    cd /var/www/ || exit
+    echo "Creating backup..."
+    tar -czvf Pterodactyl_Nightcore_Themebackup.tar.gz pterodactyl
+
     echo "Installing theme..."
-    cd /var/www/pterodactyl
-    rm -r Pterodactyl_Nightcore_Theme
+    cd /var/www/pterodactyl || exit
+    rm -rf Pterodactyl_Nightcore_Theme
     git clone https://github.com/NoPro200/Pterodactyl_Nightcore_Theme.git
-    cd Pterodactyl_Nightcore_Theme
-    rm /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css
-    rm /var/www/pterodactyl/resources/scripts/index.tsx
+
+    cd Pterodactyl_Nightcore_Theme || exit
+    rm -f /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css
+    rm -f /var/www/pterodactyl/resources/scripts/index.tsx
+
     mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
     mv Pterodactyl_Nightcore_Theme.css /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css
-    cd /var/www/pterodactyl
 
-    curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+    echo "Installing Node.js 18 and dependencies..."
+    curl -sL https://deb.nodesource.com/setup_18.x | bash -
     apt update
     apt install -y nodejs
 
-    npm i -g yarn
-    yarn
+    npm install -g yarn
+    yarn install
 
-    cd /var/www/pterodactyl
+    echo "Building panel..."
     yarn build:production
-    sudo php artisan optimize:clear
-
-
+    php artisan optimize:clear
 }
 
-installThemeQuestion(){
+installThemeQuestion() {
     while true; do
-        read -p "Are you sure that you want to install the theme [y/n]? " yn
+        read -rp "Are you sure that you want to install the theme [y/n]? " yn
         case $yn in
-            [Yy]* ) installTheme; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
+            [Yy]* ) installTheme; break ;;
+            [Nn]* ) exit ;;
+            * ) echo "Please answer y or n." ;;
         esac
     done
 }
 
-repair(){
-    bash <(curl https://raw.githubusercontent.com/NoPro200/Pterodactyl_Nightcore_Theme/main/repair.sh)
+repair() {
+    bash <(curl -s https://raw.githubusercontent.com/NoPro200/Pterodactyl_Nightcore_Theme/main/repair.sh)
 }
 
-restoreBackUp(){
+restoreBackUp() {
     echo "Restoring backup..."
-    cd /var/www/
-    tar -xvf Pterodactyl_Nightcore_Themebackup.tar.gz
-    rm Pterodactyl_Nightcore_Themebackup.tar.gz
+    cd /var/www/ || exit
+    tar -xzvf Pterodactyl_Nightcore_Themebackup.tar.gz
+    rm -f Pterodactyl_Nightcore_Themebackup.tar.gz
 
-    cd /var/www/pterodactyl
+    cd /var/www/pterodactyl || exit
+    yarn install
     yarn build:production
-    sudo php artisan optimize:clear
+    php artisan optimize:clear
 }
-echo "Copyright (c) 2024 Angelillo15 and NoPro200"
-echo "This program is free software: you can redistribute it and/or modify"
-echo ""
-echo ""
+
+# Menu
+echo "============================================"
+echo "  Pterodactyl Noi Theme Installer"
+echo "  (c) 2025 NoiMC"
+echo "============================================"
 echo "[1] Install theme"
 echo "[2] Restore backup"
-echo "[3] Repair panel (use if you have an error in the theme installation)"
+echo "[3] Repair panel"
 echo "[4] Exit"
+echo "--------------------------------------------"
 
-read -p "Please enter a number: " choice
-if [ $choice == "1" ]
-    then
-    installThemeQuestion
-fi
-if [ $choice == "2" ]
-    then
-    restoreBackUp
-fi
-if [ $choice == "3" ]
-    then
-    repair
-fi
-if [ $choice == "4" ]
-    then
-    exit
-fi
+read -rp "Please enter a number: " choice
+case $choice in
+    1) installThemeQuestion ;;
+    2) restoreBackUp ;;
+    3) repair ;;
+    4) exit ;;
+    *) echo "Invalid option!" ;;
+esac
